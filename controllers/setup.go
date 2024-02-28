9a224b8cd38f84147b5f060cb3e1a31b4d602f18
Vladimir <sciphilib@gmail.com>
2024-02-29 00:33:53 +0700

    Added some logging

--- controllers/setup.go
+++ controllers/setup.go
@@ -1,7 +1,10 @@
 package controllers
 
 import (
+	"fmt"
+	"log"
 	"net/http"
+	"time"
 
 	"github.com/gorilla/mux"
 )
@@ -14,5 +17,24 @@ func New() http.Handler {
 	router.HandleFunc("/users/registration", RegisterUser).Methods("POST")
 	router.HandleFunc("/users/authentication", AuthenticateUser).Methods("POST")
 
-	return router
+	loggedRouter := Logger(router)
+
+	return loggedRouter
+}
+
+func Logger(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		start := time.Now()
+
+		log.Printf("Started %s %s", r.Method, r.URL.Path)
+		log.Println("Headers:")
+		for name, values := range r.Header {
+			valueString := fmt.Sprintf("%s: %s", name, values[0])
+			log.Println(valueString)
+		}
+
+		next.ServeHTTP(w, r)
+
+		log.Printf("Completed %s in %v", r.URL.Path, time.Since(start))
+	})
 }
